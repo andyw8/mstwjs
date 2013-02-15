5eaed8a49d8a67eddddae9809257fd503e42e777
Andy Waite <github.aw@andywaite.com>
2013-02-15 11:29:59 +0000

    New project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mstwjs::Application.config.secret_token = '0a85eee83d978160df9150fca9c1f63e7eeff30bb31f6047b4efa2f305742d48234576232833c6358cd816057cc38ff4e85378b182da6f6a4f65d22bbe991d0c'
