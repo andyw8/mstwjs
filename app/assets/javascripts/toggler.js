85142ce782bf8ebef2c8884c64d4930f46aab28e
Andy Waite <github.aw@andywaite.com>
2013-02-19 13:54:39 +0000

    Use module pattern

--- app/assets/javascripts/toggler.js
+++ app/assets/javascripts/toggler.js
@@ -1,41 +1,44 @@
-var Toggler = function() {
-  this.detailSelector = ".detail";
-  this.linkSelector = ".detail_toggle";
-  this.hiddenClass = "hidden";
-  this.hideText = "Hide Details";
-  this.showText = "Show Details";
-  this.container = ".topics_container";
-};
+var Toggler = (function() {
+  var totalTogglers = 0;
+  var Toggler = function() {
+    this.detailSelector = ".detail";
+    this.linkSelector = ".detail_toggle";
+    this.hiddenClass = "hidden";
+    this.hideText = "Hide Details";
+    this.showText = "Show Details";
+    this.container = ".topics_container";
+  };
 
-Toggler.prototype = {
-  init: function() {
-    // inside the jQuery event handler, this refers to the event, so we must
-    // re-assign the origin 'this' so we can call it from within the event handler
-    var self = this;
-    $(this.container).on('click', this.linkSelector, function(event) {
-      self.toggleOnClick(event);
-    });
-  },
+  Toggler.prototype = {
+    init: function() {
+      var toggler = this;
+      $(this.container).on('click', this.linkSelector, function(event) {
+        toggler.toggleOnClick(event);
+      });
+    },
 
-  toggleOnClick: function(event){
-    this.$link = $(event.target);
-    if ( this.isDetailHidden() ) {
-      this.$link.text( this.hideText );
-    } else {
-      this.$link.text( this.showText );
+    toggleOnClick: function(event){
+      this.$link = $(event.target);
+      if ( this.isDetailHidden() ) {
+        this.$link.text( this.hideText );
+      } else {
+        this.$link.text( this.showText );
+      }
+      this.detailElement().toggleClass(this.hiddenClass);
+      event.preventDefault();
+    },
+
+    detailElement: function() {
+      return this.$link.parent().find(this.detailSelector);
+    },
+
+    isDetailHidden: function() {
+      return this.detailElement().hasClass(this.hiddenClass);
     }
-    this.detailElement().toggleClass(this.hiddenClass);
-    event.preventDefault();
-  },
+  };
 
-  detailElement: function() {
-    return this.$link.parent().find(this.detailSelector);
-  },
-
-  isDetailHidden: function() {
-    return this.detailElement().hasClass(this.hiddenClass);
-  }
-};
+  return Toggler;
+})();
 
 $(function() {
   var toggler = new Toggler();
